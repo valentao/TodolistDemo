8470b4d69f10a98f4bb70c44b1b0d6b9aa830ebc
Ondrej <valentao1291@gmail.com>
2023-08-03 14:43:19 +0200

    configure application project

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace TodolistDemo.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
