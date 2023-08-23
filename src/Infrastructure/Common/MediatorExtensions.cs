91a127217860c3e79ad4969d9fb7f41243a94404
Ondrej <valentao1291@gmail.com>
2023-08-23 14:38:13 +0200

    add MediatorExtensions

---
+++ src/Infrastructure/Common/MediatorExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodolistDemo.Domain.Common;
+
+namespace MediatR;
+
+public static class MediatorExtensions
+{
+    public static async Task DispatchDomainEvents(this IMediator mediator, DbContext context)
+    {
+        var entities = context.ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity);
+
+        var domainEvents = entities
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+
+        entities.ToList().ForEach(e => e.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await mediator.Publish(domainEvent);
+        }
+
+
+        /*
+         *         var entities = context.ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity);
+
+        var domainEvents = entities
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+
+        entities.ToList().ForEach(e => e.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await mediator.Publish(domainEvent);
+         */
+    }
+}
