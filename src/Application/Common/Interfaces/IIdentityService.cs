5f703600a22a078dd59c902507b4403dfe33c92a
Ondrej <valentao1291@gmail.com>
2023-08-23 16:52:38 +0200

    add Identity support

---
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using TodolistDemo.Application.Common.Models;
+
+namespace TodolistDemo.Application.Common.Interfaces;
+
+public interface IIdentityService
+{
+    Task<string?> GetUserNameAsync(string userId);
+
+    Task<bool> IsInRoleAsync(string userId, string role);
+
+    Task<bool> AuthorizeAsync(string userId, string policyName);
+
+    Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
+
+    Task<Result> DeleteUserAsync(string userId);
+}
