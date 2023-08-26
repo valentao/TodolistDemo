9ff439ee5ae7cf9f98c061ac19d31acdffb47a1b
Ondrej <valentao1291@gmail.com>
2023-08-26 18:58:58 +0200

    adding db context

---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using MediatR;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using TodolistDemo.Application.Common.Interfaces;
+using TodolistDemo.Domain.Entities;
+using TodolistDemo.Infrastructure.Identity;
+using TodolistDemo.Infrastructure.Persistence.Interceptors;
+
+namespace TodolistDemo.Infrastructure.Persistence;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
+{
+
+    private readonly IMediator _mediator;
+    private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions,
+        IMediator mediator, 
+        AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor)
+        :base (options, operationalStoreOptions)
+    {
+        _mediator = mediator;
+        _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+    }
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _mediator.DispatchDomainEvents(this);
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
